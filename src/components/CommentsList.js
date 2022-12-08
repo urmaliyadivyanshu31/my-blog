c6a211ffe815b96bcd7b5bc88bd46c7cb2b39739
Divyanshu Urmaliya <urmaliyadiv04@gmail.com>
2022-12-08 23:55:42 +0530

    Added sentence

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,6 +1,6 @@
 const CommentsList = ({ comments }) => (
     <>
-    <h3>Comments:</h3>
+    <h3>Comments Here:</h3>
     {comments.map(comment => (
         <div className="comment" key={comment.postedBy + ': ' + comment.text}>
             <h4>{comment.postedBy}</h4>
