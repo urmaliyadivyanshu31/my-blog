1b3d1a05777f371380e513e18376b59db0f20717
Divyanshu Urmaliya <urmaliyadiv04@gmail.com>
2023-02-08 23:49:11 +0530

    Added Configs

--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -31,46 +31,18 @@ const articles = [
         name: 'mongodb',
         title: 'Learn MongoDB',
         content: [
-            `Today is the day I talk about something which scares most people: resumes.
-            In reality, I'm not sure why people have such a hard time with proin congue
-            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
-            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
-            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
-            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
-            sodales purus euismod.We are here to change it the world the way it works through
-            micro pieces and this is the product we want to launch.`,
-            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `MongoDB is a popular, open-source NoSQL database that uses a document-oriented data model to store data. This means that data is stored as a collection of documents, rather than as a collection of rows in a table. MongoDB is designed to handle large amounts of data, with high scalability and performance. It also offers features such as automatic sharding for horizontal scaling, and replica sets for high availability. Additionally, MongoDB supports a wide range of programming languages and is easy to use, making it a popular choice for many modern web applications.`,
         ]
     },
     {
         name: 'javascript',
         title: 'Learn JS',
         content: [
-            `Today is the day I talk about something which scares most people: resumes.
-            In reality, I'm not sure why people have such a hard time with proin congue
-            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
-            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
-            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
-            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
-            sodales purus euismod.`,
-            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
-            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
-        ]
+            `JavaScript is a high-level, dynamic programming language that is widely used for building web applications and adding interactivity to websites. It is primarily used on the client-side, but can also be run on the server-side with technologies like Node.js. JavaScript is known for its versatility and can be used to perform tasks ranging from basic form validation to complex single-page applications and real-time communication. The syntax of JavaScript is influenced by programming languages like C and Java, making it easy for programmers to learn. JavaScript is supported by all major browsers and is an essential part of the web development stack.
+
+
+
+            `,]
     },  
 ];
 
